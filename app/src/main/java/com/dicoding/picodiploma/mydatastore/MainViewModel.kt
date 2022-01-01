96bd96e1cf500d065e190ea5c640853b8174b15e
Zaenurrochman <zaenur.rochman98@gmail.com>
2022-01-01 13:34:22 +0700

    use datastore to save preference theme

---
+++ app/src/main/java/com/dicoding/picodiploma/mydatastore/MainViewModel.kt
@@ -0,0 +1,22 @@
+package com.dicoding.picodiploma.mydatastore
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val pref: SettingPreferences) : ViewModel() {
+
+    fun getThemeSettings(): LiveData<Boolean> {
+        return pref.getThemeSetting().asLiveData()
+    }
+
+    fun saveThemeSettting(isDarkModeActive: Boolean) {
+        viewModelScope.launch {
+            pref.saveThemeSetting(isDarkModeActive)
+        }
+    }
+
+
+}
